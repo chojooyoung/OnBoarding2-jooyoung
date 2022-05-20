69145fd5e988dd8c7b63769bbc4c45aec9dbeab1
chojooyoung <jzo09@naver.com>
2022-05-20 16:20:54 +0900

    ✨ feat: mobx 연결

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
+import { Provider } from "mobx-react";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { store } from "./state";
@@ -8,14 +9,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
